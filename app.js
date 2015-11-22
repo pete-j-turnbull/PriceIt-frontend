b8b8bfb3a4088f66754209403a1d5aae34a43052
Pete Turnbull <pete.j.turnbull@gmail.com>
2015-11-22 01:04:06 +0000

    Enabled CORS using koa-cors

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var fs = require('co-fs');
 var koa = require('koa');
 var route = require('koa-route');
+var cors = require('koa-cors');
+
 var app = module.exports = koa();
+app.use(cors());
 
 app.use(route.get('/features', getFeatures));
 app.use(route.get('/price', getPrice));
-app.use(route.get('/', main));
+app.use(route.get('/index', main));
 
 
 function *main() {
-	this.body = yield fs.readFile('./index.html');
+	this.body = yield fs.readFile('./index.html', {encoding: 'utf8'});
+	console.log(this.body);
 }
 
 function *getFeatures() {
