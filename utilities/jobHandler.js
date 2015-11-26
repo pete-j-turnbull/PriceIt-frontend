6eea10a048561f54ae3d4d715500b1f30262aa02
Pete Turnbull <pete.j.turnbull@gmail.com>
2015-11-26 10:20:28 -0500

    setup jobhandler for rich

--- utilities/jobHandler.js
+++ utilities/jobHandler.js
@@ -1,15 +1,15 @@
 var config = require('../config/config');
-var zerorpc = require('zerorpc');
+//var zerorpc = require('zerorpc');
 var Promise = require('bluebird');
 
-var client = new zerorpc.Client();
-client.connect('tcp://127.0.0.1:4242');
+//var client = new zerorpc.Client();
+//client.connect('tcp://127.0.0.1:4242');
 
 module.exports.invoke = function *(msgObj) {
 	return new Promise(function (resolve, reject) {
-		client.invoke('job', msgObj, function(e, response, more) {
-			resolve(JSON.parse(response));
-		});
+		//client.invoke('job', msgObj, function(e, response, more) {
+		//	resolve(JSON.parse(response));
+		//});
 	});
 	
 }
