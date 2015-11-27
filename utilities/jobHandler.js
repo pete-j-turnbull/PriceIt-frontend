eb7682834c35735b6e5a4de291288ba07e328f5c
Pete Turnbull <pete.j.turnbull@gmail.com>
2015-11-27 03:14:22 -0500

    Indentation fix in jobHandler

--- utilities/jobHandler.js
+++ utilities/jobHandler.js
@@ -7,12 +7,12 @@ client.connect('tcp://127.0.0.1:4242');
 
 module.exports.invoke = function *(msgObj) {
     return new Promise(function (resolve, reject) {
-        client.invoke('job', msgObj, function(e, response, more) {
+	client.invoke('job', msgObj, function(e, response, more) {
             try {
-                resolve(JSON.parse(response));
-            } catch (err) {
-                reject({success: false, result: err});
+	        resolve(JSON.parse(response));
+	    } catch (err) {
+                reject({ success: false, result: err });
             }
         });
-    });	
+    });
 };
