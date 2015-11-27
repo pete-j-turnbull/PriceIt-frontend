c316792b30389930a663fb94b91b66fd6c8ea188
Pete Turnbull <pete.j.turnbull@gmail.com>
2015-11-26 19:02:33 -0500

    Added try catch on jobhandler for release

--- utilities/jobHandler.js
+++ utilities/jobHandler.js
@@ -1,15 +1,18 @@
 var config = require('../config/config');
-//var zerorpc = require('zerorpc');
+var zerorpc = require('zerorpc');
 var Promise = require('bluebird');
 
-//var client = new zerorpc.Client();
-//client.connect('tcp://127.0.0.1:4242');
+var client = new zerorpc.Client();
+client.connect('tcp://127.0.0.1:4242');
 
 module.exports.invoke = function *(msgObj) {
-	return new Promise(function (resolve, reject) {
-		//client.invoke('job', msgObj, function(e, response, more) {
-		//	resolve(JSON.parse(response));
-		//});
-	});
-	
-}
+    return new Promise(function (resolve, reject) {
+	client.invoke('job', msgObj, function(e, response, more) {
+            try {
+	        resolve(JSON.parse(response));
+	    } catch (err) {
+                reject({ success: false, result: err });
+            }
+        });
+    });
+};
